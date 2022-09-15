8139d4951bc7dcb423b82ca0396055ebbdd566ba
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-09-15 13:50:10 +0200

    changes to pom, added working module-info, can now open App directly from App.java

---
+++ time-master/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module timeMaster {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens timeMaster to javafx.fxml;
+    exports timeMaster;
+}
--- time-master/src/main/module-info.java
+++
@@ -1,7 +0,0 @@
-// module timeMaster {
-//     requires javafx.controls;
-//     requires javafx.fxml;
-
-//     opens timeMaster to javafx.fxml;
-//     exports timeMaster;
-// }

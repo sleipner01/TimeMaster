f2a00b38b01861c1ebfeff7ff7f022546b54d23d
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-09-15 11:08:48 +0200

    created App file configured for timemaster.fxml

--- time-master/src/main/java/timeMaster/App.java
+++ time-master/src/main/java/timeMaster/App.java
@@ -1,13 +1,23 @@
 package timeMaster;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("timeMaster.fxml"));
+        Parent parent = fxmlLoader.load();
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }

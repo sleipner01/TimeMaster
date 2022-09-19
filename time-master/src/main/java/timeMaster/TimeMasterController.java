3672bfeae5fd207b9ade4f01b881e81cff489580
Amalie Mansåker <amalie.mansaker@gmail.com>
2022-09-19 11:15:09 +0200

    Fiksa klokke inn og ut slik at fillagring og -lesing fungerer

--- time-master/src/main/java/timeMaster/TimeMasterController.java
+++ time-master/src/main/java/timeMaster/TimeMasterController.java
@@ -3,21 +3,25 @@ package timeMaster;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 
 public class TimeMasterController {
 
     private Employee chosenEmployee;
+    private Workday chosenWorkday;
     private Path saveDirPath = Paths.get(System.getProperty("user.dir"), "lagring");
     private TimeMasterFileHandler timeMasterFileHandler = new TimeMasterFileHandler(saveDirPath);
 
     @FXML MenuButton chooseEmployee;
     @FXML DatePicker chooseDate;
+    @FXML TextField inputHr, inputMin;
 
     
     private ArrayList<Employee> employees = new ArrayList<>();
@@ -30,11 +34,25 @@ public class TimeMasterController {
 
     @FXML void handleRegisterTime() {
         try {
-            
+            LocalDate date = chooseDate.getValue();
+            LocalTime chosenTime = LocalTime.of(Integer.parseInt(this.inputHr.getText()), Integer.parseInt(this.inputMin.getText()));
+            if (this.chosenEmployee.getWorkdays().stream().anyMatch(e -> e.getDate().equals(date))) {
+                this.chosenWorkday = this.chosenEmployee.getDate(date);
+                this.chosenWorkday.setTimeOut(chosenTime); 
+                this.saveEmployees();
+            }
+            else {
+                this.chosenEmployee.addWorkday(new Workday(date, chosenTime)); 
+                this.saveEmployees();
+            }
+            this.inputHr.clear();
+            this.inputMin.clear();
         } catch (Exception e) {
             // TODO: handle exception
         }
     }
+    
+
 
     private void saveEmployees () {
         //Lagre alle ansatte

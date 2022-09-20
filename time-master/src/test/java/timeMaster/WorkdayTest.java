7d4132738d969775b943235fc074fe2d5581da82
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-09-20 11:51:11 +0200

    minor cleanup in workdaysTest

--- time-master/src/test/java/timeMaster/WorkdayTest.java
+++ time-master/src/test/java/timeMaster/WorkdayTest.java
@@ -3,7 +3,6 @@ package timeMaster;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 import java.time.LocalTime;

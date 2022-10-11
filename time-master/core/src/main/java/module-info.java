158c23ccadcba482d06e9e827d53635a98dcd9f0
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2022-10-11 20:56:44 +0200

    added dependency for jackson-modules

--- time-master/core/src/main/java/module-info.java
+++ time-master/core/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module timeMaster.core {
 
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
     opens timeMaster.core;
     
     exports timeMaster.core;

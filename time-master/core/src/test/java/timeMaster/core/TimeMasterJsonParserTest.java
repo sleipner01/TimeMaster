75b1a8c1cdd6f9900cabfb50a9cda071e6d84b23
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2022-10-11 20:59:09 +0200

    beginning on tests for json parsing

---
+++ time-master/core/src/test/java/timeMaster/core/TimeMasterJsonParserTest.java
@@ -0,0 +1,23 @@
+package timeMaster.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TimeMasterJsonParserTest {
+    
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void writeToJSON() {
+        
+    }
+
+    @Test
+    public void readFromJSON() {
+        
+    }
+
+}

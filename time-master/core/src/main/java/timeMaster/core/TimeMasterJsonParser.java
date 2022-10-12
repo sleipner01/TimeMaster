a4b75da062b57e164370b4c3e40aa622e7e45d00
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2022-10-12 19:23:34 +0200

    cleaned up json parser
    removed deprecated main method and prints

--- time-master/core/src/main/java/timeMaster/core/TimeMasterJsonParser.java
+++ time-master/core/src/main/java/timeMaster/core/TimeMasterJsonParser.java
@@ -31,7 +31,6 @@ public class TimeMasterJsonParser {
 
     public ArrayList<Employee> read() {
         var employees = new ArrayList<Employee>();
-
         try {
             employees = this.mapper.readValue(
                     new File(filePath),
@@ -42,26 +41,7 @@ public class TimeMasterJsonParser {
             e.printStackTrace();
         }
 
-        for (var employee : employees) {
-            System.out.print(employee.toString());
-            System.out.println(employee.getWorkdays().toString());
-        }
-
         return employees;
     }
-
-    // public static void main(String[] args) {
-    //     var employees = new ArrayList<Employee>();
-    //     for (int i = 0; i < 50; i++) {
-    //         var employee = new Employee("test" + (i+1));
-    //         employee.addWorkday(new Workday());
-    //         employees.add(employee);
-    //     }
-    //     var path = Paths.get(System.getProperty("user.dir"), "time-master/core/timeMasterSaveFiles");
-    //     var parser = new TimeMasterJsonParser(path);
-
-    //     parser.write(employees);
-    //     parser.read();
-    // }
 
 }

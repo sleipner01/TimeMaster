12ae0f2c6d2ecafe5052e60dfa22c626bcb4b220
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-10-13 20:52:24 +0200

    Configured pom files to allow for FXML testing
    -.-
    
    Issue: #24

--- time-master/fxui/src/test/java/timeMaster/fxui/AppTest.java
+++ time-master/fxui/src/test/java/timeMaster/fxui/AppTest.java
@@ -2,14 +2,43 @@ package timeMaster.fxui;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
-public class AppTest 
-{
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import org.testfx.framework.junit5.ApplicationTest;
+
+/**
+ * TestFX App test
+ */
+public class AppTest extends ApplicationTest {
+
+    private Parent root;
+    private TimeMasterController controller;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("timeMaster.fxml"));
+        root = fxmlLoader.load();
+        controller = fxmlLoader.getController();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public Parent getRootNode() {
+        return root;
+    }
+
 
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test() {
+        assertTrue(true);
     }
+
 }
+

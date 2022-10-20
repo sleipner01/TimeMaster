17526e5ac0e4619dae7d571f7a93384df971c1c3
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2022-10-20 22:19:00 +0200

    added server from example
    added server from example in documentation,
    changed package names and junit version

---
+++ time-master/rest/src/test/java/no/it1901/groups2022/gr2227/timemaster/reset/RestTest.java
@@ -0,0 +1,49 @@
+package no.it1901.groups2022.gr2227.timemaster.rest;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class RestTest {
+
+    private HttpServer server;
+    private WebTarget target;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        // start the server
+        server = Main.startServer();
+        // create the client
+        Client c = ClientBuilder.newClient();
+
+        // uncomment the following line if you want to enable
+        // support for JSON in the client (you also have to uncomment
+        // dependency on jersey-media-json module in pom.xml and Main.startServer())
+        // --
+        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
+
+        target = c.target(Main.BASE_URI);
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        server.stop();
+    }
+
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    @Test
+    public void testGetIt() {
+        String responseMsg = target.path("api").request().get(String.class);
+        assertEquals("Got it!", responseMsg);
+    }
+}

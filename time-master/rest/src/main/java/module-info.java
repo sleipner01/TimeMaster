edc13159fb914ab4ee561a8fc3693f4f62e66342
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2022-10-20 21:19:04 +0200

    created rest-api module

---
+++ time-master/rest/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module no.it1901.groups2022.gr2227.timemaster.rest {
+
+  opens no.it1901.groups2022.gr2227.timemaster.rest;
+  
+  exports no.it1901.groups2022.gr2227.timemaster.rest;
+}
---
+++ time-master/rest/src/test/java/no/it1901/groups2022/gr2227/timemaster/rest/RestTest.java
@@ -0,0 +1,5 @@
+package no.it1901.groups2022.gr2227.timemaster.rest;
+
+public class RestTest {
+
+}

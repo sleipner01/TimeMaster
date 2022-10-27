b2947d2de9cda082870390fc646638f0f2605713
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2022-10-27 14:41:18 +0200

    made rest api self contained
    Co-authored-by: amalieem <amalieem@users.noreply.github.com>

--- time-master/rest/src/main/java/no/it1901/groups2022/gr2227/timemaster/rest/Rest.java
+++ time-master/rest/src/main/java/no/it1901/groups2022/gr2227/timemaster/rest/Rest.java
@@ -1,14 +1,11 @@
 package no.it1901.groups2022.gr2227.timemaster.rest;
 
-import no.it1901.groups2022.gr2227.timemaster.core.Employee;
-import no.it1901.groups2022.gr2227.timemaster.core.TimeMaster;
-import no.it1901.groups2022.gr2227.timemaster.mixin.Mixin;
+import com.fasterxml.jackson.databind.JsonNode;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -16,11 +13,7 @@ import jakarta.ws.rs.Produces;
 @Path("api")
 public class Rest {
 
-  final TimeMaster timeMaster = new TimeMaster("employees.json");
-  final ObjectMapper mapper = new ObjectMapper()
-    .addMixIn(Employee.class, Mixin.class)
-    .enable(SerializationFeature.INDENT_OUTPUT)
-    .registerModule(new JavaTimeModule());
+  FileHandler fileHandler = new FileHandler("employees.json");
 
   @Path("test")
   @GET
@@ -31,36 +24,42 @@ public class Rest {
   @Path("employees")
   @Produces("application/json")
   @GET
-  public String getEmployees() {
-    String res = null;
-    timeMaster.readEmployees();
-
+  public JsonNode getEmployees() {
     try {
-      res = mapper.writeValueAsString(timeMaster.getEmployees());
+      return fileHandler.readFile();
     } catch (Exception e) {
       e.printStackTrace();
     }
 
-    return res;
+    return null;
+  }
+
+  @Path("employees")
+  @Produces("text/plain")
+  @Consumes("application/json")
+  @PUT
+  public void createEmployee(String req) {
+    try {
+      fileHandler.write(fileHandler.parseString(req));;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   @Path("employees/{name}")
   @Produces("application/json")
   @GET
-  public String getEmployeeByName(@PathParam("name") String name) {
-    String res = null;
-    timeMaster.readEmployees();
-
+  public JsonNode getEmployeeByName(@PathParam("name") String name) {
     try {
-      for (Employee employee : timeMaster.getEmployees()) {
-        if (employee.getName().toLowerCase().equals(name.toLowerCase())) {
-          res = mapper.writeValueAsString(employee);
+      for (JsonNode node : fileHandler.readFile()) {
+        if (node.get("name").textValue().toLowerCase().equals(name.toLowerCase())) {
+          return node;
         }
       }
     } catch (Exception e) {
       e.printStackTrace();
     }
-
-    return res;
+    return null;
   }
+  
 }

bcc8e331b330aead415a1e4a3f64aac82b8f3c96
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-10-27 13:45:37 +0200

    Progress: Workday changed to feature LocalDateTime
    Added methods to use LocalDateTime, in stead of splitting to LocalDate and LocalTime.
    
    Issue: #80

--- time-master/core/src/main/java/no/it1901/groups2022/gr2227/timemaster/core/Workday.java
+++ time-master/core/src/main/java/no/it1901/groups2022/gr2227/timemaster/core/Workday.java
@@ -1,39 +1,46 @@
 package no.it1901.groups2022.gr2227.timemaster.core;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Workday {
   
-  private LocalDate date;
-  private LocalTime timeIn;
-  private LocalTime timeOut;
+  private LocalDateTime timeIn;
+  private LocalDateTime timeOut;
   
   public Workday() {}
   
   public Workday(LocalDate date, LocalTime timeIn) {
-    this.date = date;
+    this.timeIn = LocalDateTime.of(date, timeIn);
+  }
+
+  public Workday(LocalDateTime timeIn) {
     this.timeIn = timeIn;
   }
   
-  public LocalDate getDate() { 
-    return date; 
-  }
-  
-  public LocalTime getTimeIn() { 
+  public LocalDateTime getTimeIn() { 
     return timeIn; 
   }
   
-  public LocalTime getTimeOut() { 
+  public LocalDateTime getTimeOut() { 
     return timeOut;
   }
+
+  public boolean isTimedOut() {
+    return this.timeOut != null;
+  }
+
+  public void setTimeOut(LocalDate dateOut,LocalTime timeOut) {
+    this.timeOut = LocalDateTime.of(dateOut, timeOut);
+  }
   
-  public void setTimeOut(LocalTime timeOut) {
+  public void setTimeOut(LocalDateTime timeOut) {
     this.timeOut = timeOut;
   }
   
   @Override
   public String toString() {
-    return this.getDate().toString() + "," + this.getTimeIn() + "," + this.getTimeOut();
+    return this.getTimeIn() + "," + this.getTimeOut();
   }
 }

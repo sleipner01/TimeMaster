eb6f6875700ef77c7f23093cac8b44140ea615b8
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2022-10-28 14:51:04 +0200

    created client PUT and POST request
    Co-authored-by: amalieem <amalieem@users.noreply.github.com>

--- time-master/core/src/main/java/no/it1901/groups2022/gr2227/timemaster/core/ApiHandler.java
+++ time-master/core/src/main/java/no/it1901/groups2022/gr2227/timemaster/core/ApiHandler.java
@@ -1,21 +1,27 @@
 package no.it1901.groups2022.gr2227.timemaster.core;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class ApiHandler {
 
   final String baseURL = "http://localhost:8080/api/";
   private TimeMasterJsonParser jsonParser = new TimeMasterJsonParser();
   
-  private String getResponse(String path) throws IOException {
+  // https://happycoding.io/tutorials/java-server/rest-api
+  private HttpURLConnection setConnection(String path, String requestMethod) throws IOException { 
     HttpURLConnection connection = (HttpURLConnection) new URL(baseURL + path).openConnection();
+    connection.setRequestMethod(requestMethod);
+    return connection;
+  }
 
-    connection.setRequestMethod("GET");
+  private String getResponse(String path) throws IOException {
+    HttpURLConnection connection = setConnection(path, "GET");
     int responseCode = connection.getResponseCode();
     String response = "";
     if (responseCode == HttpURLConnection.HTTP_OK) {
@@ -25,9 +31,38 @@ public class ApiHandler {
         response += "\n";
       }
       scanner.close();
-      }
-      return response;
     }
+    return response;
+  }
+
+  private void request(String path, String req, String reqMethod) throws IOException{
+    HttpURLConnection connection = setConnection(path, reqMethod);
+    connection.setDoOutput(true);
+    connection.setRequestProperty("Content-Type", "application/json");
+    OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+    writer.write(req);
+    System.out.println(req);
+    writer.flush();
+
+    int responseCode = connection.getResponseCode();
+    System.out.println(responseCode);
+    switch (responseCode) {
+      case HttpURLConnection.HTTP_OK:
+      case HttpURLConnection.HTTP_CREATED:
+        System.out.println(reqMethod + " request was successful.");
+        break;
+      default:
+        System.out.println(reqMethod + " request was not successful.");
+        break;
+    }
+  }
+
+  public void postRequest(String path, String req) throws IOException {
+    HttpURLConnection connection = setConnection(path, "POST");
+    connection.setDoOutput(true);
+    connection.setRequestProperty("Content-Type", "application/json");
+
+  }
 
   public Employee getEmployee(String name) throws IOException {
     return this.jsonParser.readEmployee(this.getResponse(name));
@@ -39,6 +74,22 @@ public class ApiHandler {
 
   public ArrayList<Workday> getWorkdays(String name) throws IOException {
     return this.jsonParser.readWorkdays(this.getResponse(name));
+  }
+
+  public void createEmployee(Employee employee) {
+    try {
+      request("employees", this.jsonParser.write(employee),"PUT");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+    public void updateEmployee(Employee employee) {
+    try {
+      request("employees/" + employee.getId(), this.jsonParser.write(employee),"POST");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 }

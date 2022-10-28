01450855c80d3cdc636d9a9b8698a8ea3191f844
Håvard Solberg Nybøe <haavasny@stud.ntnu.no>
2022-10-28 20:18:34 +0200

    fixed test to work with connection error

--- time-master/core/src/test/java/no/it1901/groups2022/gr2227/timemaster/core/TimeMasterTest.java
+++ time-master/core/src/test/java/no/it1901/groups2022/gr2227/timemaster/core/TimeMasterTest.java
@@ -3,6 +3,7 @@ package no.it1901.groups2022.gr2227.timemaster.core;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.net.ConnectException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -20,7 +21,7 @@ public class TimeMasterTest {
   
   @Test
   public void createEmployeeTest() {
-    assertDoesNotThrow(() -> timeMaster.createEmployee("Anna"));
+    assertThrows(ConnectException.class,() -> timeMaster.createEmployee("Anna"));
     assertThrows(IllegalArgumentException.class, () -> timeMaster.createEmployee(""));
 
   }
@@ -32,24 +33,24 @@ public class TimeMasterTest {
 
   @Test
   public void chooseEmployeeTest() {
-    timeMaster.createEmployee("Anna");
+    assertThrows(ConnectException.class,() -> timeMaster.createEmployee("Anna"));
     assertDoesNotThrow(() -> timeMaster.setChosenEmployee(0));
     assertThrows(IndexOutOfBoundsException.class, () -> timeMaster.setChosenEmployee(1));
   }
 
   @Test
   public void clockEmployeeInOutTest() {
-    timeMaster.createEmployee("Anna");
+    assertThrows(ConnectException.class,() -> timeMaster.createEmployee("Anna"));
     assertThrows(IllegalStateException.class, () -> timeMaster.clockEmployeeInOut(LocalDate.EPOCH, LocalTime.MIDNIGHT));
     assertDoesNotThrow(() -> timeMaster.setChosenEmployee(0));
-    assertDoesNotThrow(() -> timeMaster.clockEmployeeInOut(LocalDate.EPOCH, LocalTime.MIDNIGHT));
+    assertThrows(ConnectException.class, () -> timeMaster.clockEmployeeInOut(LocalDate.EPOCH, LocalTime.MIDNIGHT));
   }
 
   @Test void autoClockEmployeeInOutTest() {
-    timeMaster.createEmployee("Anna");
+    assertThrows(ConnectException.class,() -> timeMaster.createEmployee("Anna"));
     assertThrows(IllegalStateException.class, () -> timeMaster.autoClockEmployeeInOut());
     assertDoesNotThrow(() -> timeMaster.setChosenEmployee(0));
-    assertDoesNotThrow(() -> timeMaster.autoClockEmployeeInOut());
+    assertThrows(ConnectException.class, () -> timeMaster.autoClockEmployeeInOut());
   }
 
 }

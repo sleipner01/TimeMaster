e186847f8a6ce4ca9e0484d57f1c3590cffac1e2
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-10-28 13:30:23 +0200

    Created test for toString
    -.-
    
    Issue: #48

--- time-master/core/src/test/java/no/it1901/groups2022/gr2227/timemaster/core/WorkdayTest.java
+++ time-master/core/src/test/java/no/it1901/groups2022/gr2227/timemaster/core/WorkdayTest.java
@@ -3,7 +3,6 @@ package no.it1901.groups2022.gr2227.timemaster.core;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 
@@ -37,24 +36,15 @@ public class WorkdayTest {
   @Test
   @DisplayName("Test toString")
   public void testToString() {
-    assertTrue(true);
-    // LocalDateTime timeIn = this.getTimeIn();
-    // String dayOfWeek = timeIn.getDayOfWeek().toString();
-    // String dayOfMonth = String.valueOf(timeIn.getDayOfMonth());
-    // String month = timeIn.getMonth().toString();
-    // String year = String.valueOf(timeIn.getYear());
-    // String inHour = String.valueOf(timeIn.getHour());
-    // String inMinute = String.valueOf(timeIn.getMinute());
+    String correctTime1 = "THURSDAY 1 JANUARY 1970            |         00:00         |              ";
+    String correctTime2 = "THURSDAY 1 JANUARY 1970            |         00:00         |         01:00";
+    Workday workday = new Workday(LocalDateTime.of(1970, 1, 1, 0, 0, 0));
 
-    // String date = dayOfWeek + " " + dayOfMonth + " " + month + " " + year;
-    // String stampIn = inHour + ":" + inMinute;
-    // String stampOut = "";
+    assertEquals(workday.toString(), correctTime1);
 
-    // if(timeOut != null) {
-    //   stampOut = String.valueOf(timeOut.getHour()) + ":" + String.valueOf(timeOut.getMinute());
-    // } 
+    workday.setTimeOut(LocalDateTime.of(1970, 1, 1, 1, 0, 0));
 
-    // return date + " | " + stampIn + " | " + stampOut;
+    assertEquals(workday.toString(), correctTime2);
   }
   
 }

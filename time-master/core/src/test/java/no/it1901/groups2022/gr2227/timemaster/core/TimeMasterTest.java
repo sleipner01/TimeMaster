662a0dd306fd0ea2b83a797b3e9df8b3712258eb
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-11-04 15:14:19 +0100

    Added state test.
    Also refreshed testsyntax
    
    Issue: #106
    Author: @karengb
    Co-author: @magnueb

--- time-master/core/src/test/java/no/it1901/groups2022/gr2227/timemaster/core/TimeMasterTest.java
+++ time-master/core/src/test/java/no/it1901/groups2022/gr2227/timemaster/core/TimeMasterTest.java
@@ -3,6 +3,7 @@ package no.it1901.groups2022.gr2227.timemaster.core;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -13,15 +14,20 @@ import org.junit.jupiter.api.Test;
 public class TimeMasterTest {
 
   TimeMaster timeMaster;
+  String testName = "Test Name";
 
   @BeforeEach
   public void setup() {
     timeMaster = new TimeMaster();
     timeMaster.setApplicationInTestState();
+  }
+
+  private void createTestingEmployee() {
     try {
-      timeMaster.createEmployee("Test Person");
-    } catch (IOException e) {
-      System.out.println("API probably not connected");
+      timeMaster.createEmployee(testName);
+    } catch (Exception e) {
+      // Should never run
+      System.err.println("Somehow the API calls are on...");
     }
   }
 
@@ -37,19 +43,29 @@ public class TimeMasterTest {
   }
 
   @Test
+  public void setApplicationInTestStateTest() {
+    assertDoesNotThrow(() -> timeMaster.createEmployee(testName));
+    timeMaster.setApplicationInTestState();
+    assertTrue(timeMaster.getEmployees().size() == 0);
+  }
+
+  @Test
   public void chooseEmployeeTest() {
+    createTestingEmployee();
     assertDoesNotThrow(() -> timeMaster.setChosenEmployee(timeMaster.getEmployees().get(0)));
-    assertEquals("Test Person", timeMaster.getChosenEmployee().getName());
+    assertEquals(testName, timeMaster.getChosenEmployee().getName());
   }
 
   @Test
   public void clockEmployeeInOutTest() {
+    createTestingEmployee();
     assertDoesNotThrow(() -> timeMaster.setChosenEmployee(timeMaster.getEmployees().get(0)));
     assertDoesNotThrow(() -> timeMaster.clockEmployeeInOut(LocalDateTime.now()));
   }
 
   @Test
   void autoClockEmployeeInOutTest() {
+    createTestingEmployee();
     assertDoesNotThrow(() -> timeMaster.setChosenEmployee(timeMaster.getEmployees().get(0)));
     assertDoesNotThrow(() -> timeMaster.autoClockEmployeeInOut());
   }

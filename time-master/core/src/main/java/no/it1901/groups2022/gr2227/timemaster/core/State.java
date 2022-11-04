da08a25e6b0fc146402f7fed53bf5e903d1d1353
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-11-04 12:55:46 +0100

    New state LOCAL added
    LOCAL state added to disable API calls, and can be wident to support local filesystems.
    Javadoc for States.java added.
    
    Issue: #106
    Co-author: @karengb

--- time-master/core/src/main/java/no/it1901/groups2022/gr2227/timemaster/core/State.java
+++ time-master/core/src/main/java/no/it1901/groups2022/gr2227/timemaster/core/State.java
@@ -1,6 +1,17 @@
 package no.it1901.groups2022.gr2227.timemaster.core;
 
+/**
+ * Enum with alloweed states for the application.
+ *
+ * <code>PRODUCTION</code>  - All functionality enabled. 
+ * <code>LOCAL</code>       - API turned off. 
+ *                            Only internal lists and/or local files. 
+ * <code>TEST</code>        - Limits functionality for testing purposes.
+ *                            I.E: API calls will only be run in classes that 
+ *                            solely rely on the API.
+ */
 enum State {
   PRODUCTION,
+  LOCAL,
   TEST
 }

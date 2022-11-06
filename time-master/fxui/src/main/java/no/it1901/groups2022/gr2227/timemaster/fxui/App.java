9d9ec96227f5c39bb35a30298a61fa17a8db1641
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-11-06 02:08:31 +0100

    Javadoc and checkstyle fix for App
    Zero warnings for App
    
    Issue: #100

--- time-master/fxui/src/main/java/no/it1901/groups2022/gr2227/timemaster/fxui/App.java
+++ time-master/fxui/src/main/java/no/it1901/groups2022/gr2227/timemaster/fxui/App.java
@@ -1,14 +1,20 @@
 package no.it1901.groups2022.gr2227.timemaster.fxui;
 
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
+/**
+ * App.java is the start class for the TimeMaster application.
+ * It will initialize the user interface and connect to the controller.
+ *
+ * @version %I%, %G%
+ * @since 1.0 
+ * @see TimeMasterController
+ */
 public class App extends Application {
   
   @Override

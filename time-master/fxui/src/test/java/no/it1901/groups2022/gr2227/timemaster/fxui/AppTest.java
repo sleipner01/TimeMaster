180a6ab039851808d2c289d44b0d0d1ba9bf20fc
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-11-06 19:35:13 +0100

    More subtests
    Better testing for adding employee
    
    Issue: #93, #92

--- time-master/fxui/src/test/java/no/it1901/groups2022/gr2227/timemaster/fxui/AppTest.java
+++ time-master/fxui/src/test/java/no/it1901/groups2022/gr2227/timemaster/fxui/AppTest.java
@@ -2,6 +2,7 @@ package no.it1901.groups2022.gr2227.timemaster.fxui;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import java.io.File;
@@ -15,16 +16,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import no.it1901.groups2022.gr2227.timemaster.core.Employee;
 
 import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
 import org.testfx.matcher.control.LabeledMatchers;
 
 
@@ -67,35 +73,92 @@ public class AppTest extends ApplicationTest {
     return root;
   }
 
+
+
   @Test
   public void testAppConstructor() {
     assertDoesNotThrow(() -> new App());
   }
+
+
 
   @Test
   public void testThatControllerIsPresent() {
     assertNotNull(this.controller);
   }
 
+
+
+  @Test
+  public void testApiStatusOffline() {
+    assumeTrue((!controller.getApiStatus()) && (!controller.getIsUsingApi()));
+    Text status = lookup("#statusTextApi").query();
+    FxAssert.verifyThat(status, s -> s.getText().equals("Offline"));
+    Circle indicator = lookup("#statusIndicatorApi").query();
+    FxAssert.verifyThat(indicator, i -> i.getFill().equals(Color.GRAY));
+  }
+
+
+
+  @Test
+  public void testApiStatusAvailable() {
+    assumeTrue(controller.getApiStatus() && (!controller.getIsUsingApi()));
+    Text status = lookup("#statusTextApi").query();
+    FxAssert.verifyThat(status, s -> s.getText().equals("Available"));
+    Circle indicator = lookup("#statusIndicatorApi").query();
+    FxAssert.verifyThat(indicator, i -> i.getFill().equals(Color.BLUE));
+  }
+
+
+
+  @Test
+  public void testApiStatusOnline() {
+    assumeTrue(controller.getApiStatus());
+    controller.setApplicationInProductionState();
+    Text status = lookup("#statusTextApi").query();
+    FxAssert.verifyThat(status, s -> s.getText().equals("Online"));
+    Circle indicator = lookup("#statusIndicatorApi").query();
+    FxAssert.verifyThat(indicator, i -> i.getFill().equals(Color.GREEN));
+  }
+
+
+
   @ParameterizedTest
   @MethodSource
   public void testTabs(String input) {
-      clickOn(LabeledMatchers.hasText(input));
+      assertDoesNotThrow(() -> clickOn(LabeledMatchers.hasText(input)));
   }
 
   private static Stream<String> testTabs() {
       return Stream.of("Stamp In", "Check Hours Worked", "Add New Employee");
   }
 
+
+
   @Test
   public void testAddEmployee() {
+    // Add valid employee
     clickOn(LabeledMatchers.hasText("Add New Employee"));
     clickOn("#newEmployeeName").write(testName);
     clickOn("#addNewEmployeeButton");
-    clickOn(LabeledMatchers.hasText("Stamp In"));
 
-    FxAssert.verifyThat(testName, LabeledMatchers.hasText(testName));
+    TextField nameInput = lookup("#newEmployeeName").query();
+    FxAssert.verifyThat(nameInput, n -> n.getText().length() == 0);
+    Text status = lookup("#addStatus").query();
+    FxAssert.verifyThat(status, s -> s.getFill().equals(Color.GREEN));
+    FxAssert.verifyThat(status, s -> s.getText().length() > 0);
+    ListView<Employee> listView = lookup("#chooseEmployeeListView").query();
+    ObservableList<Employee> employeesList = listView.getItems();
+    assertTrue(employeesList.size() > 0);
+    assertTrue(employeesList.get(0).getName().equals(testName));
+
+    // Add invalid employee
+    // clickOn("#newEmployeeName").write(testName);
+    clickOn("#addNewEmployeeButton");
+    FxAssert.verifyThat("OK", NodeMatchers.isVisible());
   }
+
+
 
   @Test
   public void testAutoStampIn() {
@@ -113,32 +176,26 @@ public class AppTest extends ApplicationTest {
     FxAssert.verifyThat(indicator, i -> i.getFill().equals(Color.GREEN));
   }
 
-  @Test
-  public void testApiStatusOffline() {
-    assumeTrue((!controller.getApiStatus()) && (!controller.getIsUsingApi()));
-    Text status = lookup("#statusTextApi").query();
-    FxAssert.verifyThat(status, s -> s.getText().equals("Offline"));
-    Circle indicator = lookup("#statusIndicatorApi").query();
-    FxAssert.verifyThat(indicator, i -> i.getFill().equals(Color.GRAY));
-  }
+
 
   @Test
-  public void testApiStatusAvailable() {
-    assumeTrue(controller.getApiStatus() && (!controller.getIsUsingApi()));
-    Text status = lookup("#statusTextApi").query();
-    FxAssert.verifyThat(status, s -> s.getText().equals("Available"));
-    Circle indicator = lookup("#statusIndicatorApi").query();
-    FxAssert.verifyThat(indicator, i -> i.getFill().equals(Color.BLUE));
-  }
+  public void testManualStampIn() {
+    // Adding an employee
+    clickOn(LabeledMatchers.hasText("Add New Employee"));
+    clickOn("#newEmployeeName").write(testName);
+    clickOn("#addNewEmployeeButton");
+    clickOn(LabeledMatchers.hasText("Stamp In"));
+    clickOn(LabeledMatchers.hasText(testName));
+    clickOn("#autoRegisterTimeButton");
 
-  @Test
-  public void testApiStatusOnline() {
-    assumeTrue(controller.getApiStatus());
-    controller.setApplicationInProductionState();
-    Text status = lookup("#statusTextApi").query();
-    FxAssert.verifyThat(status, s -> s.getText().equals("Online"));
-    Circle indicator = lookup("#statusIndicatorApi").query();
+    Text status = lookup("#statusText").query();
+    FxAssert.verifyThat(status, s -> s.getText().equals("Active"));
+    Circle indicator = lookup("#statusIndicator").query();
     FxAssert.verifyThat(indicator, i -> i.getFill().equals(Color.GREEN));
   }
+
+
+
+
 
 }

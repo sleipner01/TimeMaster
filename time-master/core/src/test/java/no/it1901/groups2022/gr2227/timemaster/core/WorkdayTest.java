400661c5230f485c69a66baeb47e3921afae528a
Amalie Mansåker <amalie.mansaker@gmail.com>
2022-11-06 13:06:28 +0100

    Made tests for all public classes, 98% test coverage #90

--- time-master/core/src/test/java/no/it1901/groups2022/gr2227/timemaster/core/WorkdayTest.java
+++ time-master/core/src/test/java/no/it1901/groups2022/gr2227/timemaster/core/WorkdayTest.java
@@ -3,11 +3,15 @@ package no.it1901.groups2022.gr2227.timemaster.core;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -54,14 +58,67 @@ public class WorkdayTest {
   public void testGetTimeIn() {
     assertEquals(testTimeIn, workday.getTimeIn());
   }
+
+  @Test
+  @DisplayName("Test formatting of timeIn")
+  public void testFormatTimeIn() {
+    String correctFormat = "Thu. Jan 01 1970 - 12:00";
+    LocalDateTime timeIn = LocalDateTime.of(1970, Month.JANUARY, 1, 12, 0);
+    workday = new Workday(timeIn);
+    assertEquals(correctFormat, workday.getTimeInAsFormattedString());
+  }
+
+  @Test
+  @DisplayName("Test formatting of timeOut")
+  public void testFormatTimeOut() {
+    String correctFormat = "Thu. Jan 01 1970 - 12:00";
+    LocalDateTime timeIn = LocalDateTime.of(1970, Month.JANUARY, 1, 11, 0);
+    LocalDateTime timeOut = LocalDateTime.of(1970, Month.JANUARY, 1, 12, 0);
+    workday = new Workday(timeIn);
+    assertThrows(IllegalStateException.class, () -> {
+      workday.getTimeOutAsFormattedString();
+    });
+    workday.setTimeOut(timeOut);
+    assertEquals(correctFormat, workday.getTimeOutAsFormattedString());
+  }
+
+  @Test
+  @DisplayName("Test setTimeOut that takes in LocalDate and LocalTime")
+  public void testSetTimeOutDateAndTime() {
+    LocalDate testDate = LocalDate.now();
+    LocalTime testTime = LocalTime.now();
+    workday.setTimeOut(testDate, testTime);
+
+    LocalTime outBeforeIn = LocalTime.now();
+    LocalTime inAfterOut = LocalTime.now();
+    workday = new Workday(testDate, inAfterOut);
+    assertThrows(IllegalArgumentException.class, () -> {
+      workday.setTimeOut(testDate, outBeforeIn);
+    });
+  }
   
   @Test
-  @DisplayName("Test setTimeOut()")
-  public void setTimeOutTest() {
+  @DisplayName("Test setTimeOut that takes in LocalDateTime")
+  public void testSetTimeOutDateTime() {
     workday.setTimeOut(testTimeOut);
     assertEquals(testTimeOut, workday.getTimeOut());
+
+    LocalDateTime outBeforeIn = LocalDateTime.now();
+    LocalDateTime inAfterOut = LocalDateTime.now();
+    workday = new Workday(inAfterOut);
+    assertThrows(IllegalArgumentException.class, () -> {
+      workday.setTimeOut(outBeforeIn);
+    });
   } 
-  
+
+  @Test
+  @DisplayName("Test if is timed out") 
+  public void testTimedOut() {
+    assertFalse(workday.isTimedOut());
+    workday.setTimeOut(testTimeOut);
+    assertTrue(workday.isTimedOut());
+  }
+
   @Test
   @DisplayName("Test toString")
   public void testToString() {

439cb84a82aa873174ef22fd56cd308ebab3d6c1
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-11-10 11:19:09 +0100

    Added javadoc plugins
    Both for individual modules, and for an aggregated report in time-master/target/site/javadocs
    
    Issue: #88

--- time-master/fxui/src/main/java/module-info.java
+++ time-master/fxui/src/main/java/module-info.java
@@ -1,3 +1,10 @@
+/**
+ * UI Module. Contains files for UI layout and controlling the application and visual elements.
+ *
+ * @author Magnus Byrkjeland
+ * @since 1.0
+ * @version 1.0
+ */
 module no.it1901.groups2022.gr2227.timemaster.fxui {
     requires no.it1901.groups2022.gr2227.timemaster.core;
     requires javafx.controls;

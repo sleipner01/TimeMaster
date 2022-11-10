c53dfa5f7e8c7e3199cded50a35129dfa6d3d643
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-11-10 14:05:30 +0100

    Minor fix

--- time-master/fxui/src/main/java/no/it1901/groups2022/gr2227/timemaster/fxui/App.java
+++ time-master/fxui/src/main/java/no/it1901/groups2022/gr2227/timemaster/fxui/App.java
@@ -30,7 +30,7 @@ public class App extends Application {
   /**
    * Start method
    * 
-   * @param args
+   * @param args  arguments
    */
   public static void main(String[] args) {
     launch();

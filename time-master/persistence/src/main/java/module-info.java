12ffa29508036145dc2d64164a7642bb2332d02f
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-11-15 13:48:50 +0100

    Minor change to module-info

--- time-master/persistence/src/main/java/module-info.java
+++ time-master/persistence/src/main/java/module-info.java
@@ -7,7 +7,7 @@
  */
 module no.it1901.groups2022.gr2227.timemaster.persistence {
   requires com.fasterxml.jackson.core;
-  requires com.fasterxml.jackson.databind;
+  requires transitive com.fasterxml.jackson.databind;
   requires com.fasterxml.jackson.datatype.jsr310;
 
   exports no.it1901.groups2022.gr2227.timemaster.persistence;

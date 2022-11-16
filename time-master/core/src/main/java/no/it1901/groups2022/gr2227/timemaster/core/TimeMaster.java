1d095990e778dfbe35b820ea9aa5a47a198666eb
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-11-16 14:10:23 +0100

    Javadoc fix in TimeMaster

--- time-master/core/src/main/java/no/it1901/groups2022/gr2227/timemaster/core/TimeMaster.java
+++ time-master/core/src/main/java/no/it1901/groups2022/gr2227/timemaster/core/TimeMaster.java
@@ -427,6 +427,7 @@ public class TimeMaster {
    *
    * @return                        List of workdays.
    * @throws IllegalStateException  If no employee is set.
+   * @throws IOException            If API call fails.
    */
   public ArrayList<Workday> getEmployeeWorkdayHistory() 
       throws IllegalStateException, IOException {
@@ -458,7 +459,7 @@ public class TimeMaster {
    * @throws IllegalArgumentException   If the workday does not exist at the employee.
    *                                    If the timestamps comes in conflict with some other
    *                                    workdays at the employee or is not valid.
-   * @throws IOException                If the APU call fails.
+   * @throws IOException                If the API call fails.
    *
    * @see Employee#editWorkday(Workday, LocalDateTime, LocalDateTime)
    * @see TimeMaster#setChosenEmployee(Employee)

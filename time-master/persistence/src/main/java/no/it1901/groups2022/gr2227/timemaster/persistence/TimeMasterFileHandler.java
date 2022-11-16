ce8e1f4ee1054ea90b6ae45029b7ab374b8aacd0
Magnus Byrkjeland <magnueb@stud.ntnu.no>
2022-11-16 13:59:18 +0100

    Fixed checkstyle-warnings in persistence module
    -.-
    
    Issue: #34

--- time-master/persistence/src/main/java/no/it1901/groups2022/gr2227/timemaster/persistence/TimeMasterFileHandler.java
+++ time-master/persistence/src/main/java/no/it1901/groups2022/gr2227/timemaster/persistence/TimeMasterFileHandler.java
@@ -2,6 +2,11 @@ package no.it1901.groups2022.gr2227.timemaster.persistence;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+/**
+ * TimeMasterFileHandler is an interface that describes 
+ * what a filehandler-implementation for the TimeMaster application
+ * must contain.
+ */
 public interface TimeMasterFileHandler {
 
   /**
